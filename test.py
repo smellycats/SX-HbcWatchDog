ff73c208b3d5621cc99f3dc5c1ecffa8092c4a3b
SmellyCat <smellycat2014@foxmail.com>
2015-12-08 16:04:40 +0800

    更新 完善短信发送功能

--- test.py
+++ test.py
@@ -10,11 +10,11 @@ class WatchDog(object):
     def __init__(self):
         self.kkdd_dict = {
             '441302': u'惠城区',
-            '441303': u'惠阳',
+            '441303': u'惠阳县',
             '441305': u'大亚湾',
-            '441322': u'博罗',
-            '441323': u'惠东',
-            '441324': u'龙门'
+            '441322': u'博罗县',
+            '441323': u'惠东县',
+            '441324': u'龙门县'
         }
 
         self.date_flag = arrow.now().replace(days=-1).date()
@@ -29,13 +29,31 @@ class WatchDog(object):
             'password': '123456',
             'token': 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ0MzI1NjcyMiwiaWF0IjoxNDQzMjQ5NTIyfQ.eyJzY29wZSI6WyJzY29wZV9nZXQiLCJoemhiY19nZXQiXSwidWlkIjoyM30.Qga6zksBXBu8Aq9zVBb7tsR_vQFI4A7IfzdgMvGEfrw'
         }
-
+        self.sms_ini = {
+            'host': '127.0.0.1',
+            'port': 8090,
+            'username': 'test1',
+            'password': '123456',
+            'token': 'eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ0MzI1NjcyMiwiaWF0IjoxNDQzMjQ5NTIyfQ.eyJzY29wZSI6WyJzY29wZV9nZXQiLCJoemhiY19nZXQiXSwidWlkIjoyM30.Qga6zksBXBu8Aq9zVBb7tsR_vQFI4A7IfzdgMvGEfrw'
+        }
     def __del__(self):
         pass
 
     def sms_send(self, content, mobiles):
         """发送短信"""
-        return 0
+        headers = {
+            'content-type': 'application/json',
+            'access_token': self.cgs_ini['token']
+        }
+        url = 'http://%s:%s/sms' % (
+            self.sms_ini['host'], self.sms_ini['port'])
+        data = {'content': content, 'mobiles': mobiles}
+        try:
+            r = requests.get(url, headers=headers, data=json.dumps(data))
+            if r.status_code == 201:
+                return json.loads(r.text)
+        except Exception as e:
+            print e
 
     def get_hbc_count(self, date, kkdd):
         """根据日期，地点获取黄标车数据量"""
@@ -57,25 +75,25 @@ class WatchDog(object):
         finally:
             return count_int
 
-    def get_count_dict(self, date):
+    def get_count_info(self, date):
         """根据日期获取各地区黄标车数据量"""
-        self.count_dict = {}
+        # 统计信息字符串
+        count_info = ''
         for i in self.kkdd_dict.keys():
-            self.count_dict[self.kkdd_dict[i]] = self.get_hbc_count(date, i)
-        return self.count_dict
+            c = self.get_hbc_count(date, i)
+            count_info += '%s: %s; ' % (self.kkdd_dict[i], str(c))
+        return count_info
         
     def run(self):
-        print 'run'
         while 1:
             try:
                 # 当前时间
                 t = arrow.now()
                 # 如果当前日期大于记录日期并且当前小时大于额定发送小时
                 if t.date() > self.date_flag and t.hour >= self.send_time:
-                    count_dict = self.get_count_dict(t.date())
-                    self.sms_send('%s: %s' % (str(t.date()), str(count_dict)[1:-1]),
-                                  self.mobiles_list)
-                    print '[%s]%s' % (str(t), str(count_dict))
+                    count_info = u'[惠州黄标车][%s] %s' % (
+                        str(t.date()), self.get_count_info(str(t.date())))
+                    self.sms_send(count_info, self.mobiles_list)
                     self.date_flag = t.date()
             except Exception as e:
                 print e
